e3446573edc96cd96eb2bf25452de24fa8b4dafd
Caleb Irwin <me@calebirwin.ca>
2024-03-05 01:10:13 -0600

    cleaning up

--- kit/svelte.config.js
+++ kit/svelte.config.js
@@ -4,16 +4,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
 
 	vitePlugin: {
 		inspector: true
 	},
 	kit: {
-		adapter: adapter()
-		// csrf: false
+		adapter: adapter(),
+		csrf: false
 	}
 };
 export default config;
