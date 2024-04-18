b71217b20916a9968debb32ffe9afe59bb5b15fa
Caleb Irwin <me@calebirwin.ca>
2024-04-17 20:27:21 -0600

    basic home + nav bar

--- svelte/src/routes/app/+page.svelte
+++ svelte/src/routes/app/+page.svelte
@@ -2,19 +2,52 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	import Search from 'lucide-svelte/icons/search';
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1 text-center">App Home</h1>
+<div class="container h-full mx-auto flex flex-col justify-center items-center">
+	<div>
+		<h1 class="h1 text-center mb-2">Home</h1>
 		<p class="text-center">
 			Welcome <span class="font-semibold">{data.user.username}</span>. Your permission level is
 			<span class="font-semibold">{data.user.permissionLevel}</span>.
-			<br />
-			{#if data.user.permissionLevel === 'admin'}
-				<a href="/app/admin" class="text-center underline">Admin Panel</a>
-			{/if}
-			<a href="/app/shelf" class="text-center underline">Shelf</a>
 		</p>
+	</div>
+	<form class="p-2 m-2 mb-3 h-20 max-w-2xl form w-full flex">
+		<div class="input-group input-group-divider grid-cols-[1fr_auto_auto]">
+			<input type="text" placeholder="Search Query" disabled />
+			<select disabled>
+				<option>All</option>
+				<option>QB</option>
+			</select>
+			<button class="variant-filled-primary w-16" disabled>
+				<Search />
+			</button>
+		</div>
+	</form>
+	<div class="w-full grid grid-cols-1 lg:grid-cols-2">
+		<a href="/app/shelf" class="card card-hover p-6 m-2">
+			<h3 class="font-semibold text-2xl">Shelf</h3>
+		</a>
+		<a href="/app/" class="card p-6 m-2 cursor-not-allowed text-surface-400">
+			<h3 class="font-semibold text-2xl">Quality Assurance</h3>
+		</a>
+		<a href="/app/" class="card p-6 m-2 cursor-not-allowed text-surface-400">
+			<h3 class="font-semibold text-2xl">QuickBooks</h3>
+		</a>
+		<a href="/app/" class="card p-6 m-2 cursor-not-allowed text-surface-400">
+			<h3 class="font-semibold text-2xl">Guild</h3>
+		</a>
+		<a href="/app/" class="card p-6 m-2 cursor-not-allowed text-surface-400">
+			<h3 class="font-semibold text-2xl">SPRichards</h3>
+		</a>
+		<a href="/app/" class="card p-6 m-2 cursor-not-allowed text-surface-400">
+			<h3 class="font-semibold text-2xl">Shopify</h3>
+		</a>
+		{#if data.user.permissionLevel === 'admin'}
+			<a href="/app/admin" class="card card-hover p-6 m-2">
+				<h3 class="font-semibold text-2xl">Admin Panel</h3>
+			</a>
+		{/if}
 	</div>
 </div>
--- svelte/src/routes/app/Bar.svelte
+++ svelte/src/routes/app/Bar.svelte
@@ -2,12 +2,20 @@
 	import Bttn from '$lib/Button.svelte';
 	import { client } from '$lib/client';
 	import { AppBar } from '@skeletonlabs/skeleton';
+	import Home from 'lucide-svelte/icons/home';
+	import LogOut from 'lucide-svelte/icons/log-out';
 </script>
 
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">(icon)</svelte:fragment>
-	(title)
+	<svelte:fragment slot="lead"
+		><a href="/" class="btn btn-icon variant-filled-primary">
+			<Home />
+		</a></svelte:fragment
+	>
+	<a href="/" class="font-semibold text-2xl">OEM3</a>
 	<svelte:fragment slot="trail">
-		<Bttn action={client.user.logout} invalidateAll>Logout</Bttn>
+		<Bttn action={client.user.logout} class="btn btn-icon variant-filled-secondary" invalidateAll
+			><LogOut /></Bttn
+		>
 	</svelte:fragment>
 </AppBar>
