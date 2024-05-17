0de4ffe4485bc3c304522449da7fe21909e03365
Caleb Irwin <me@calebirwin.ca>
2024-05-16 19:36:09 -0600

    read csv

--- server/src/routers/qb/worker.ts
+++ server/src/routers/qb/worker.ts
@@ -1,18 +1,55 @@
 import { work } from "../../utils/workerBase";
-
+import Papa from "papaparse";
+import { PromisePool } from "@supercharge/promise-pool";
+import { qb } from "./table";
+import { eq, sql } from "drizzle-orm";
 declare var self: Worker;
 
 work(
   self,
-  async ({}) => {
-    return 100;
+  async ({ fileBlob }) => {
+    return atob(fileBlob.slice(fileBlob.indexOf("base64,") + 7)).split("\n")
+      .length;
   },
-  async ({ incrementProgress }) => {
+  async ({ incrementProgress, fileBlob, db }) => {
+    const res = Papa.parse(
+      atob(fileBlob.slice(fileBlob.indexOf("base64,") + 7)),
+      { header: true }
+    );
+
+    const getQBItem = db.query.qb
+      .findFirst({ where: eq(qb.qbId, sql.placeholder("qbId")) })
+      .prepare("qb-item");
+
     let i = 0;
-    while (i < 10) {
-      incrementProgress(10);
-      await Bun.sleep(100);
-      i++;
-    }
+
+    await PromisePool.withConcurrency(10)
+      .for(res.data as QbItemRaw[])
+      .onTaskFinished(() => {
+        i++;
+        if (i % 500 === 0) incrementProgress(500);
+      })
+      .process(async (item) => {
+        if (item.Type !== "Inventory Part") return;
+        const prev = await getQBItem.execute({ qbId: item.Item });
+      });
   }
 );
+
+export interface QbItemRaw {
+  Item: string;
+  Description: string;
+  Type: string;
+  Cost: string;
+  Price: string;
+  "Sales Tax Code": string;
+  "Purchase Tax Code": string;
+  "Quantity On Sales Order": string;
+  "Reorder Pt (Min)": string;
+  "Quantity On Purchase Order": string;
+  "U/M Set": string;
+  Account: string;
+  "Quantity On Hand": string;
+  "U/M": string;
+  "Preferred Vendor": string;
+}
