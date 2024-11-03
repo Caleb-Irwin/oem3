fc1676ba87249beefc9025251647e4da924ce8dd
Caleb Irwin <me@calebirwin.ca>
2024-11-02 22:47:53 -0400

    in progress smart input

--- svelte/src/routes/app/unifiedItems/[id]/+page.svelte
+++ svelte/src/routes/app/unifiedItems/[id]/+page.svelte
@@ -9,6 +9,13 @@
 
 	export let data: PageData;
 	const res = query(client.unifiedItems.item, { id: parseInt(data.id) });
+	let alt: boolean = !!(
+		$res?.defaultAltConversionFactor !== undefined ||
+		$res?.altUm !== undefined ||
+		$res?.altPriceCents !== undefined ||
+		$res?.qbAlt ||
+		$res?.shopifyAlt
+	);
 </script>
 
 {#if $res === undefined}
@@ -30,19 +37,34 @@
 				<EnhancedItemRow {smartChangeset} key="guildInventory">Guild Inventory</EnhancedItemRow>
 				<EnhancedItemRow {smartChangeset} key="guildFlyer">Guild Flyer</EnhancedItemRow>
 			</div>
-			<h3 class="h3 pt-2 p-1">Unit of Measure</h3>
-			<div class="flex">
-				<SmartInput {smartChangeset} key="defaultUm" />
+			<div class="flex items-center">
+				<h3 class="h3 pt-4 p-1">Unit of Measure</h3>
+				<div class="flex-grow"></div>
+				<button
+					class="btn btn-sm h-8 variant-ghost-secondary text-secondary-500"
+					on:click={() => (alt = !alt)}>{alt ? 'Hide' : 'Show'} Alternate UM</button
+				>
+			</div>
+			<div class="flex flex-col lg:flex-row p-2 pt-0 gap-2">
+				<SmartInput {smartChangeset} key="defaultUm" example="ea" />
+				{#if alt}
+					<SmartInput {smartChangeset} key="defaultAltConversionFactor" example="12" />
+					<SmartInput {smartChangeset} key="altUm" example="pk" />
+				{/if}
 			</div>
 			<h3 class="h3 pt-2 p-1">QuickBooks</h3>
-			<div class="grid gap-2 grid-cols-1 sm:grid-cols-2 px-2">
+			<div class="grid gap-2 grid-cols-1 {alt ? 'sm:grid-cols-2' : ''} px-2">
 				<EnhancedItemRow {smartChangeset} key="qb">QuickBooks</EnhancedItemRow>
-				<EnhancedItemRow {smartChangeset} key="qbAlt">QuickBooks Alternate</EnhancedItemRow>
+				{#if alt}
+					<EnhancedItemRow {smartChangeset} key="qbAlt">QuickBooks Alternate</EnhancedItemRow>
+				{/if}
 			</div>
 			<h3 class="h3 pt-2 p-1">Shopify</h3>
-			<div class="grid gap-2 grid-cols-1 sm:grid-cols-2 px-2">
+			<div class="grid gap-2 grid-cols-1 {alt ? 'sm:grid-cols-2' : ''} px-2">
 				<EnhancedItemRow {smartChangeset} key="shopify">Shopify</EnhancedItemRow>
-				<EnhancedItemRow {smartChangeset} key="shopifyAlt">Shopify Alternate</EnhancedItemRow>
+				{#if alt}
+					<EnhancedItemRow {smartChangeset} key="shopifyAlt">Shopify Alternate</EnhancedItemRow>
+				{/if}
 			</div>
 			<p class="whitespace-pre-wrap">Item {JSON.stringify($res, undefined, 2)}</p>
 		</div>
--- svelte/src/routes/app/unifiedItems/[id]/SmartInput.svelte
+++ svelte/src/routes/app/unifiedItems/[id]/SmartInput.svelte
@@ -1,16 +1,57 @@
 <script lang="ts">
-	import type { getSmartChangeset, SmartChangesetKeys } from './smartChangeset';
+	import { readableKeys, type getSmartChangeset, type SmartChangesetKeys } from './smartChangeset';
 
-	export let smartChangeset: Awaited<ReturnType<typeof getSmartChangeset>>, key: SmartChangesetKeys;
+	export let smartChangeset: Awaited<ReturnType<typeof getSmartChangeset>>,
+		key: SmartChangesetKeys,
+		hasColumnSettings: boolean = true,
+		example: string | undefined = undefined;
 	const value = smartChangeset.fields[key].value,
-		original = smartChangeset.fields[key].original;
+		original = smartChangeset.fields[key].original,
+		colSetValue = hasColumnSettings
+			? smartChangeset.fields[(key + 'ColumnSettings') as SmartChangesetKeys].value
+			: undefined,
+		colSetOriginal = hasColumnSettings
+			? smartChangeset.fields[(key + 'ColumnSettings') as SmartChangesetKeys].original
+			: undefined;
 </script>
 
-<input
-	type="text"
-	class="input text-center ml-1 {$original != ($value === '' ? null : $value)
-		? 'border-success-600 bg-success-300 dark:border-success-500 dark:bg-success-800'
-		: ''}"
-	bind:value={$value}
-	placeholder="null"
-/>
+{#if hasColumnSettings && colSetValue && colSetOriginal}
+	<div class="w-full">
+		<p>{readableKeys[key]}</p>
+		<div
+			class="input-group input-group-divider grid-cols-[1fr_auto] {$original !=
+				($value === '' ? null : $value) || $colSetOriginal != $colSetValue
+				? 'border-success-600 dark:border-success-500'
+				: ''}"
+		>
+			<input
+				class="text-center {$original != ($value === '' ? null : $value)
+					? ' !border-success-600 !bg-success-300 dark:!border-success-500 dark:!bg-success-800'
+					: ''}"
+				type="text"
+				bind:value={$value}
+				placeholder={example ? `ex. ${example}` : 'null'}
+			/>
+			<select
+				class="text-sm text-center {$colSetOriginal != $colSetValue
+					? ' !border-success-600 !bg-success-300 dark:!border-success-500 dark:!bg-success-800'
+					: ''}"
+				bind:value={$colSetValue}
+			>
+				<option value="automatic">Automatic</option>
+				<option value="automaticSmallChanges">Auto Small</option>
+				<option value="approval">Approval</option>
+				<option value="manual">Manual</option>
+			</select>
+		</div>
+	</div>
+{:else}
+	<input
+		type="text"
+		class="input text-center ml-1 {$original != ($value === '' ? null : $value)
+			? 'border-success-600 bg-success-300 dark:border-success-500 dark:bg-success-800'
+			: ''}"
+		bind:value={$value}
+		placeholder="null"
+	/>
+{/if}
