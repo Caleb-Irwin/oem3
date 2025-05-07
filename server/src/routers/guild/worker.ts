efc4b9470e4f6a22d709055e6dc821b4cadaaa8f
Caleb Irwin <me@calebirwin.ca>
2025-05-06 20:32:47 -0400

    updateUnifiedTable V1 done (lest updateRow)

--- server/src/routers/guild/worker.ts
+++ server/src/routers/guild/worker.ts
@@ -1,157 +1,17 @@
-import { desc, gt, isNull, or } from "drizzle-orm";
-import { KV } from "../../utils/kv";
 import { work } from "../../utils/workerBase";
-import { guildData } from "./data/table";
 import { unifiedGuild } from "./table";
-import PromisePool from "@supercharge/promise-pool";
-import { guildFlyer, guildInventory, uniref } from "../../db.schema";
-import { insertMultipleHistoryRows } from "../../utils/history";
-import { updateUnifiedGuildRow } from "./updateUnifiedGuildRow";
-import { chunk } from "../../utils/chunk";
-
+import { guildUnifier } from "./guildUnifier";
 declare var self: Worker;
 
 work({
   self,
   process: async ({ db, progress }) => {
-    progress(-1);
-    const rowsToUpdate = new Set<number>();
     await db.transaction(async (db) => {
-      const kv = new KV("unifiedGuild", db),
-        lastGuildUpdate = 0; //TODO parseInt((await kv.get("lastGuildUpdate")) ?? "0");
-      await db.delete(unifiedGuild).execute(); //TODO
-      const currentTime = Date.now();
-
-      // 1. Recently updated primary items + new items
-      const recentlyUpdated = await db.query.guildData
-        .findMany({
-          columns: { id: true, gid: true, lastUpdated: true },
-          where: gt(guildData.lastUpdated, lastGuildUpdate),
-          with: { unifiedGuild: true },
-          orderBy: desc(guildData.lastUpdated),
-        })
-        .execute();
-      if (recentlyUpdated.length > 0) {
-        await kv.set(
-          "lastGuildUpdate",
-          recentlyUpdated[0].lastUpdated.toString()
-        );
-      }
-      const toAdd: typeof recentlyUpdated = [];
-      for (const row of recentlyUpdated) {
-        if (row?.unifiedGuild?.id) {
-          rowsToUpdate.add(row.unifiedGuild.id);
-        } else {
-          toAdd.push(row);
-        }
-      }
-      await PromisePool.withConcurrency(10)
-        .for(chunk(toAdd))
-        .handleError(async (error, _, pool) => {
-          console.error(error);
-          return pool.stop();
-        })
-        .process(async (chunk) => {
-          const rows = await db
-            .insert(unifiedGuild)
-            .values(
-              chunk.map((row) => ({
-                gid: row.gid,
-                dataRow: row.id,
-                lastUpdated: row.lastUpdated,
-              }))
-            )
-            .returning({ id: unifiedGuild.id })
-            .execute();
-          rows.forEach(({ id }) => rowsToUpdate.add(id));
-          const uniRows = await db
-            .insert(uniref)
-            .values(
-              rows.map(({ id }) => ({
-                resourceType: "unifiedGuild" as const,
-                unifiedGuild: id,
-              }))
-            )
-            .returning({ uniId: uniref.uniId })
-            .execute();
-          await insertMultipleHistoryRows({
-            db,
-            resourceType: "unifiedGuild",
-            rows: chunk.map((v, i) => ({
-              uniref: uniRows[i].uniId,
-              entryType: "create",
-              data: { gid: v.gid, dataRow: v.id },
-              created: currentTime,
-            })),
-          });
-        });
-
-      // 2. Find to match or update
-      const inventoryLastUpdated = 0, //TODO
-        // parseInt(
-        //   (await kv.get("lastGuildInventoryUpdate")) ?? "0"
-        // ),
-        flyerLastUpdated = 0; //TODO
-      //   parseInt(
-      //   (await kv.get("lastGuildFlyerUpdate")) ?? "0"
-      // )
-      const toMatchOrUpdate = await db.query.unifiedGuild.findMany({
-        columns: { id: true },
-        where: or(
-          isNull(unifiedGuild.inventoryRow),
-          isNull(unifiedGuild.flyerRow),
-          gt(guildInventory.lastUpdated, inventoryLastUpdated),
-          gt(guildFlyer.lastUpdated, flyerLastUpdated)
-        ),
-        with: {
-          inventoryRowContent: {
-            columns: { lastUpdated: true },
-          },
-          flyerRowContent: {
-            columns: { lastUpdated: true },
-          },
-        },
+      await db.delete(unifiedGuild).execute(); // TODO: Remove this
+      await guildUnifier.updateUnifiedTable({
+        db,
+        progress,
       });
-      let greatestInventoryLastUpdated = inventoryLastUpdated,
-        greatestFlyerLastUpdated = flyerLastUpdated;
-      for (const row of toMatchOrUpdate) {
-        rowsToUpdate.add(row.id);
-        if (
-          row.inventoryRowContent &&
-          greatestInventoryLastUpdated < row.inventoryRowContent.lastUpdated
-        )
-          greatestInventoryLastUpdated = row.inventoryRowContent.lastUpdated;
-        if (
-          row.flyerRowContent &&
-          greatestFlyerLastUpdated < row.flyerRowContent.lastUpdated
-        )
-          greatestFlyerLastUpdated = row.flyerRowContent.lastUpdated;
-      }
-      await kv.set(
-        "lastGuildInventoryUpdate",
-        greatestInventoryLastUpdated.toString()
-      );
-      await kv.set("lastGuildFlyerUpdate", greatestFlyerLastUpdated.toString());
     });
-
-    // 3. Update matches + summaries
-    console.time("updateMatches");
-    let done = 0;
-    await PromisePool.withConcurrency(20)
-      .for(rowsToUpdate)
-      .handleError(async (error, _, pool) => {
-        console.error(error);
-        return pool.stop();
-      })
-      .process(async (id) => {
-        await db.transaction(async (db) => {
-          await updateUnifiedGuildRow(id, db);
-        });
-        done++;
-        if (done % 100 === 0) progress(done / rowsToUpdate.size);
-      });
-    console.timeEnd("updateMatches");
-
-    // 4. Done!
   },
 });
--- server/src/utils/unifier.ts
+++ server/src/utils/unifier.ts
@@ -1,85 +1,249 @@
-import { guildData, guildFlyer, guildInventory, unifiedGuild } from "../db.schema";
+import {
+  guildData,
+  guildFlyer,
+  guildInventory,
+  unifiedGuild,
+  uniref,
+} from "../db.schema";
 import { db as DB } from "../db";
-
-export type UnifiedTables = typeof unifiedGuild;
-export type PrimarySourceTables = typeof unifiedGuild | typeof guildData
-// export type SecondarySourceTables = typeof unifiedSPR
-export type OtherSourceTables = typeof guildInventory | typeof guildFlyer
-
-type CellTransformerOptions<T> = {
-    shouldMatch?: { name: string, val: T, ignore?: boolean };
-    shouldNotBeNull?: boolean;
-    neverNull?: boolean;
-    isPrice?: boolean;
-    isRef?: boolean;
-}
-
-const cellTransformer = <T extends UnifiedTables, K extends keyof T['$inferSelect']>(
-    key: K,
-    val: T['$inferSelect'][K],
-    options?: CellTransformerOptions<T['$inferSelect'][K]>
-) => {
-    return {
-        key,
-        val,
-        options,
-    }
-}
-
-export interface TableConnections<RowType, T extends PrimarySourceTables | OtherSourceTables> {
-    table: T,
-    refCol: keyof RowType;
-    recheckConnectionsOnFieldChange: string[];
-    findConnections: (row: RowType, db: typeof DB) => Promise<number[]>;
-}
-
-interface Connections<RowType> {
-    primaryTable: TableConnections<RowType, PrimarySourceTables>;
-    // secondaryTable?: TableConnections<RowType, SecondarySourceTables>;
-    otherTables: TableConnections<RowType, OtherSourceTables>[];
-}
+import { eq, isNull, type SQLWrapper, gt } from "drizzle-orm";
+import { getTableConfig } from "drizzle-orm/pg-core";
+import { chunk } from "./chunk";
+import {
+  insertMultipleHistoryRows,
+  type InsertHistoryRowOptions,
+} from "./history";
+import { KV } from "./kv";
 
 export function createUnifier<RowType, TableType extends UnifiedTables>({
-    table,
-    getRow,
-    transform,
-    connections,
+  table,
+  getRow,
+  transform,
+  connections,
+  version,
 }: {
-    table: TableType;
-    getRow: (id: number, db: typeof DB) => Promise<RowType>;
-    transform: (item: RowType, t: typeof cellTransformer) => {
-        [K in keyof TableType['$inferSelect']]: ReturnType<typeof cellTransformer> };
-    connections: Connections<RowType>;
+  table: TableType;
+  getRow: (id: number, db: typeof DB) => Promise<RowType>;
+  transform: (
+    item: RowType,
+    t: typeof cellTransformer
+  ) => {
+    [K in keyof TableType["$inferSelect"]]: ReturnType<typeof cellTransformer>;
+  };
+  connections: Connections<RowType, TableType>;
+  version: number;
 }) {
-    async function updateRow({ id, db = DB }: {
-        id: number,
-        db?: typeof DB
-    }) {
-        const item = await getRow(id, db);
-        // 1. Check primary connections
-        // 2. Check + make other connections
-        // 3. Transform
-        const transformed = transform(item, cellTransformer);
-        // 4. Apply Overrides + Find Errors
-        // 5. Update Row
-        // 6. Update History
-        throw new Error("Not implemented");
-    }
-
-    async function updateUnifiedTable({ db = DB, updateAll = false, progress }: {
-        db?: typeof DB,
-        updateAll?: boolean,
-        progress?: (progress: number) => void
-    }) {
-        if (progress) progress(-1);
-        // 1. Add missing primary rows
-        // 2. Determine which rows need to be updated
-        // 3. Update Rows
-        // 4. Batch Update History
-    }
-
+  async function updateRow({
+    id,
+    insertHistory = true,
+    db = DB,
+  }: {
+    id: number;
+    insertHistory?: boolean;
+    db?: typeof DB;
+  }): Promise<{
+    history: InsertHistoryRowOptions<TableType["$inferSelect"]>;
+  }> {
+    const item = await getRow(id, db);
+    // 1. Check primary connections
+    // 2. Check + make other connections
+    // 3. Transform
+    const transformed = transform(item, cellTransformer);
+    // 4. Apply Overrides + Find Errors
+    // 5. Update Row
+    // 6. Update History
+    // throw new Error("Not implemented");
     return {
-        updateUnifiedTable,
-        updateRow,
+      history: {} as any,
+    };
+  }
+
+  async function updateUnifiedTable({
+    db = DB,
+    updateAll = false,
+    progress,
+  }: {
+    db?: typeof DB;
+    updateAll?: boolean;
+    progress?: (progress: number) => void;
+  }) {
+    if (progress) progress(-1);
+    const unifiedTableName = getTableConfig(table).name,
+      kv = new KV("unifier/" + unifiedTableName, db),
+      newLastUpdated = Date.now(),
+      prevLastUpdated = parseInt((await kv.get("lastUpdated")) ?? "0"),
+      rowsToUpdate = new Set<number>();
+    if (parseInt((await kv.get("version")) ?? "-1") < version) {
+      updateAll = true;
     }
+
+    // 1. Add missing primary rows
+    const missingPrimaryRows = await db
+      .select()
+      .from(connections.primaryTable.table)
+      .leftJoin(
+        table,
+        eq(
+          connections.primaryTable.table.id,
+          table[connections.primaryTable.refCol] as SQLWrapper
+        )
+      )
+      .where(isNull(table.id))
+      .execute();
+    const primaryTableName = getTableConfig(
+      connections.primaryTable.table
+    ).name;
+    const rowsToInsert = missingPrimaryRows.map((v) =>
+      connections.primaryTable.newRowTransform(
+        v[primaryTableName as unknown as keyof typeof v] as any,
+        newLastUpdated
+      )
+    );
+    for (const chunkedRows of chunk(rowsToInsert)) {
+      const rows = await db
+        .insert(table)
+        .values(chunkedRows)
+        .returning({ id: table.id })
+        .execute();
+      rows.forEach(({ id }) => rowsToUpdate.add(id));
+      const uniRows = await db
+        .insert(uniref)
+        .values(
+          rows.map(({ id }) => {
+            const obj: any = {
+              resourceType: unifiedTableName as any,
+            };
+            obj[unifiedTableName] = id;
+            return obj;
+          })
+        )
+        .returning({ uniId: uniref.uniId })
+        .execute();
+      await insertMultipleHistoryRows({
+        db,
+        resourceType: unifiedTableName as any,
+        rows: chunkedRows.map((v, i) => ({
+          uniref: uniRows[i].uniId,
+          entryType: "create",
+          data: v,
+          created: newLastUpdated,
+        })),
+      });
+    }
+
+    // 2. Determine which rows need to be updated
+    if (updateAll) {
+      const allRows = await db.select({ id: table.id }).from(table).execute();
+      allRows.forEach((v) => rowsToUpdate.add(v.id));
+    } else {
+      const sourceTables = [
+        connections.primaryTable.table,
+        ...connections.otherTables.map((v) => v.table),
+      ];
+      for (const sourceTable of sourceTables) {
+        const rows = await db
+          .select({ id: table.id })
+          .from(table)
+          .innerJoin(
+            sourceTable,
+            eq(
+              table[connections.primaryTable.refCol] as SQLWrapper,
+              sourceTable.id
+            )
+          )
+          .where(gt(sourceTable.lastUpdated, prevLastUpdated));
+        rows.forEach((r) => rowsToUpdate.add(r.id));
+      }
+    }
+    // 3. Update Rows
+    if (progress) progress(0);
+    let done = 0;
+    const historyToRecord: InsertHistoryRowOptions<
+      TableType["$inferSelect"]
+    >[] = [];
+    for (const id of rowsToUpdate) {
+      const { history } = await updateRow({ id, db, insertHistory: false });
+      historyToRecord.push(history);
+      done++;
+      if (progress && done % 50 === 0) progress(done / rowsToUpdate.size);
+    }
+    if (progress) progress(1);
+
+    // 4. Batch Update History
+    for (const chunkedHistory of chunk(historyToRecord)) {
+      await insertMultipleHistoryRows({
+        db,
+        resourceType: unifiedTableName as any,
+        rows: chunkedHistory,
+      });
+    }
+    // 5. Finish
+    await kv.set("lastUpdated", newLastUpdated.toString());
+    await kv.set("version", version.toString());
+  }
+
+  return {
+    updateUnifiedTable,
+    updateRow,
+  };
+}
+
+export type UnifiedTables = typeof unifiedGuild;
+export type PrimarySourceTables = typeof unifiedGuild | typeof guildData;
+// export type SecondarySourceTables = typeof unifiedSPR
+export type OtherSourceTables = typeof guildInventory | typeof guildFlyer;
+
+type CellTransformerOptions<T> = {
+  shouldMatch?: { name: string; val: T; ignore?: boolean };
+  shouldNotBeNull?: boolean;
+  neverNull?: boolean;
+  isPrice?: boolean;
+  isRef?: boolean;
+};
+
+const cellTransformer = <
+  T extends UnifiedTables,
+  K extends keyof T["$inferSelect"]
+>(
+  key: K,
+  val: T["$inferSelect"][K],
+  options?: CellTransformerOptions<T["$inferSelect"][K]>
+) => {
+  return {
+    key,
+    val,
+    options,
+  };
+};
+
+interface TableConnection<
+  RowType,
+  UnifiedTable extends UnifiedTables,
+  T extends PrimarySourceTables | OtherSourceTables
+> {
+  table: T;
+  refCol: keyof UnifiedTable;
+  recheckConnectionsOnFieldChange: string[];
+  findConnections: (row: RowType, db: typeof DB) => Promise<number[]>;
+}
+
+interface PrimaryTableConnection<
+  RowType,
+  UnifiedTable extends UnifiedTables,
+  T extends PrimarySourceTables
+> extends TableConnection<RowType, UnifiedTable, T> {
+  newRowTransform: (
+    row: T["$inferSelect"],
+    lastUpdated: number
+  ) => UnifiedTable["$inferInsert"];
+}
+
+interface Connections<RowType, UnifiedTable extends UnifiedTables> {
+  primaryTable: PrimaryTableConnection<
+    RowType,
+    UnifiedTable,
+    PrimarySourceTables
+  >;
+  // secondaryTable?: TableConnections<RowType, SecondarySourceTables>;
+  otherTables: TableConnection<RowType, UnifiedTable, OtherSourceTables>[];
 }
