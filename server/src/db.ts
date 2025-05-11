8f005eb910f54fec7cdb7efa51834e1625b10e52
Caleb Irwin <me@calebirwin.ca>
2025-05-11 16:40:37 -0400

    revert to old driver

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,24 +1,31 @@
-import { drizzle } from "drizzle-orm/bun-sql";
-import { migrate as m } from "drizzle-orm/bun-sql/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate as m } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
 import * as schema from "./db.schema";
 import { POSTGRESQL } from "./env";
-import { SQL } from "bun";
 
-const getClient = async (depth = 0) => {
+const connect = async (depth = 0): Promise<ReturnType<typeof postgres>> => {
   if (depth > 0) console.log("Connecting to DB (try " + (depth + 1) + ")");
   try {
-    const sql = new SQL(POSTGRESQL);
+    const sql = postgres(POSTGRESQL, {
+      onnotice: (e) => {
+        if (e["code"] === "42P06" || e["code"] === "42P07") return;
+        console.warn(e);
+      },
+    });
     await sql`select 1`;
     return sql;
   } catch (e: any) {
     if ((e.code === "ECONNREFUSED" || e.code === "57P03") && depth < 60) {
       await new Promise((res) => setTimeout(res, 500));
-      return await getClient(depth + 1);
+      return await connect(depth + 1);
     } else throw e;
   }
 };
 
-export const db = drizzle({ client: await getClient(), schema });
+const client = await connect();
+
+export const db = drizzle(client, { schema });
 
 export const migrate = async () =>
   await m(db, { migrationsFolder: "./drizzle" });
