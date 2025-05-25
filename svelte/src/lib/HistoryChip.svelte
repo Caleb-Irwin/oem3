3f8b6dbda70a3283719a67d7074097c305962bbf
Caleb Irwin <me@calebirwin.ca>
2025-05-25 00:00:51 -0400

    better wrapping

--- svelte/src/lib/HistoryChip.svelte
+++ svelte/src/lib/HistoryChip.svelte
@@ -14,7 +14,7 @@
 		if (typeof value === 'string') return value;
 		if (typeof value === 'number') {
 			if (key.endsWith('Cents')) return formatCurrency(value / 100);
-			else return value;
+			else return value.toString();
 		}
 		if (typeof value === 'boolean') return value ? 'True' : 'False';
 		if (value === null) return 'NULL';
@@ -22,12 +22,30 @@
 	}
 </script>
 
-<span
-	class="chip hover:cursor-default whitespace-break-spaces variant-filled-surface m-0.5 flex flex-col lg:flex-row"
->
-	<span class="font-bold text-primary-500 pr-1"> {key}: </span>
-	{#if !create}
-		{toReadable(prev)}<span class="font-bold text-primary-500 pr-2"> -> </span>
-	{/if}
-	{toReadable(value)}
-</span>
+<div class="p-0.5">
+	<span
+		class="chip hover:cursor-default whitespace-break-spaces variant-filled-surface flex flex-col sm:flex-row m-0"
+	>
+		<span class="font-bold text-primary-500 pr-1">{key}:</span>
+		{#if !create}
+			{@render breakable(toReadable(prev).split(' '))}
+			<span class="font-bold text-primary-500 pr-2">-></span>
+		{/if}
+		{@render breakable(toReadable(value).split(' '))}
+		{#if create}
+			<span class="font-bold text-primary-500">New</span>
+		{/if}
+	</span>
+</div>
+
+{#snippet breakable(text: string[])}
+	<span>
+		{#each text as t}
+			{#if t.length > 20}
+				<span class="break-all">{t}</span>
+			{:else}
+				{t}
+			{/if}{' '}
+		{/each}
+	</span>
+{/snippet}
